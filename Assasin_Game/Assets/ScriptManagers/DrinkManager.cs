76c84d61f6de2b4ec8972cbc2ee47d11cd746a95
Szerlock <ryan.cubas21@gmail.com>
2024-07-04 22:24:44 -0600

    save

---
+++ Assasin_Game/Assets/ScriptManagers/DrinkManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrinkManager : MonoBehaviour
+{
+    // public List<string> requiredIngredients;
+
+    // private List<string> currentIngredients;
+
+    // public Text messageText;
+
+    // public void SetRequiredIngredients(List<string> ingredients){
+    //     requiredIngredients = new List<string>(ingredients);
+    // }
+
+    // public void AddIngredients(string ingredient){
+    //     if (currentIngredients.Count < 3)
+    //     {
+    //         currentIngredients.Add(ingredient);
+    //         Debug.Log("Added ingredient: " + ingredient);
+    //     }
+    //     else
+    //     {
+    //         messageText.text = "The drink is already full.";
+    //         Debug.Log("The drink is already full.");
+    //     }
+    // }
+
+    // public Boolean CheckDrink(){
+    //     for(int i = 0; i <requiredIngredients.Count; i++){
+    //         if(currentIngredients[i] != requiredIngredients[i]){
+    //             messageText.text = "The drink is incorrect";
+    //             Debug.Log("The drink is incorrect");
+    //             return false;
+    //         }
+    //     }
+
+    //     messageText.text = "correct!";
+    //     Debug.Log("The drink is correct!");
+    //     return true;
+    // }
+    // public void ResetDrink()
+    // {
+    //     currentIngredients.Clear();
+    //     messageText.text = "";
+    //     Debug.Log("Drink reset.");
+    // }
+}
--- Assasin_Game/Assets/Scripts Bar/CanvasManager.cs
+++ Assasin_Game/Assets/Scripts Bar/CanvasManager.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Scripting;
 using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
@@ -10,6 +14,73 @@ public class CanvasManager : MonoBehaviour
 
     private GameObject newCanvasInstance;
     private Canvas newCanvasComponent;
+    
+    private TMP_Text messageText;
+
+   // [SerializeField]
+    //private Button[] ingredientButtons = new Button[5];
+    private List<string> currentIngredients = new List<string>();
+
+    private List<string> requiredIngredients = new List<string>();
+
+     void Start()
+    {
+        requiredIngredients.Add("Rum");
+                requiredIngredients.Add("Vodka");
+
+        requiredIngredients.Add("Brandy");
+
+
+        // foreach (Button button in ingredientButtons)
+        // {
+        //     if (button != null)
+        //     {
+        //         string ingredientTag = button.tag;
+        //         button.onClick.AddListener(() => OnButtonClick(ingredientTag));
+        //         Debug.Log("Setting up button: " + button.name + " with tag: " + button.tag);
+        //     }
+        //     else
+        //     {   
+        //         Debug.LogWarning("Button is null in ingredientButtons array");
+        //     }
+        //     if (button.interactable)
+        //         {
+        //             Debug.Log("Button " + button.name + " is interactable.");
+        //         }
+        //         else
+        //         {
+        //             Debug.LogWarning("Button " + button.name + " is not interactable.");
+        //         }
+        // }
+        // ingredientButtons = newCanvas.GetComponentsInChildren<Button>();
+
+        // foreach (Button button in ingredientButtons)
+        // {
+        //     button.onClick.AddListener(() => OnButtonClick(button.tag));
+        // }
+    }
+
+    void SetupIngredientButtons()
+    {
+        Button[] buttons = newCanvasInstance.GetComponentsInChildren<Button>();
+        TMP_Text[] tmpTexts = newCanvasInstance.GetComponentsInChildren<TMP_Text>();
+        foreach (TMP_Text tmpText in tmpTexts)
+        {
+        if (tmpText.gameObject.CompareTag("Drink")) 
+        {
+            messageText = tmpText;
+            break; 
+        }
+        }
+        foreach (Button button in buttons)
+        {
+            string ingredientTag = button.tag;
+            button.onClick.AddListener(() => OnButtonClick(ingredientTag));
+            Debug.Log("Setting up button: " + button.name + " with tag: " + button.tag);
+        }
+
+        
+    }
 
     public void ShowNewCanvas()
     {
@@ -21,12 +92,14 @@ public class CanvasManager : MonoBehaviour
         {
             newCanvasInstance = Instantiate(newCanvas);
             newCanvasComponent = newCanvasInstance.GetComponent<Canvas>();
+
+                        SetupIngredientButtons();
+
         }
 
         newCanvasInstance.SetActive(true);
         newCanvasComponent.enabled = true;
         newCanvasInstance.GetComponent<GraphicRaycaster>().enabled = true;
-
     }
 
     public void ShowMainCanvas()
@@ -36,9 +109,56 @@ public class CanvasManager : MonoBehaviour
             newCanvasInstance.SetActive(false);
             newCanvasComponent.enabled = false;
             newCanvasInstance.GetComponent<GraphicRaycaster>().enabled = false;
+
+            Debug.Log("mainCanvas enabled: " + mainCanvas.enabled);
+        Debug.Log("mainCanvas GraphicRaycaster enabled: " + mainCanvas.GetComponent<GraphicRaycaster>().enabled);
         }
 
         mainCanvas.enabled = true;
         mainCanvas.GetComponent<GraphicRaycaster>().enabled = true;
+    }
+
+    public void OnButtonClick(string ingredient){
+                Debug.Log("Button clicked: " + ingredient);
+        
+         AddIngredient(ingredient);
+         if(currentIngredients.Count == 3){
+            CheckDrink();
+        }
+    }
+
+    public void AddIngredient(string ingredient){
+        if (currentIngredients.Count < 3)
+        {
+            currentIngredients.Add(ingredient);
+            Debug.Log("Added ingredient: " + ingredient);
+        }
+        else
+        {
+            messageText.text = "The drink is already full.";
+            Debug.Log("The drink is already full.");
+        }
+    }
+
+    public bool CheckDrink(){
+        for(int i = 0; i <requiredIngredients.Count - 1; i++){
+            if(currentIngredients[i] != requiredIngredients[i]){
+                messageText.text = "The drink is incorrect";
+                Debug.Log("The drink is incorrect");
+                return false;
+            }
+        }
+
+        messageText.text = "correct!";
+        Debug.Log("The drink is correct!");
+        ShowMainCanvas();
+        return true;
+    }
+
+    public void ResetDrink()
+    {
+        currentIngredients.Clear();
+        messageText.text = "";
+        Debug.Log("Drink reset.");
     }
 }
